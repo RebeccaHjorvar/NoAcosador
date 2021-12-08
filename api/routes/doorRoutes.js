bae12288888de85980ede707fad93de29ca4a738
ItsSteph <steph.tronier.vip@gmail.com>
2021-12-08 18:27:57 +0100

    renamed doorlist to door

--- api/routes/doorRoutes.js
+++ api/routes/doorRoutes.js
@@ -1,12 +1,12 @@
 module.exports = (app) => {
-    const doorList = require('../controllers/doorController');
+    const door = require('../controllers/doorController');
 
 app.route('/Door')
-.get(doorList.getAllDoors)
-.post(doorList.createDoor)
+.get(door.getAllDoors)
+.post(door.createDoor)
 
 app.route('/door/:id')
-.get(doorList.findDoorById)
-.put(doorList.updateDoor)
-.delete(doorList.deleteDoor)
+.get(door.findDoorById)
+.put(door.updateDoor)
+.delete(door.deleteDoor)
 };
