2ea00ab777e8ccfb4fbc4b8588f9bee4d3d6d870
roblindstrom <73818899+roblindstrom@users.noreply.github.com>
2021-12-16 22:03:01 +0100

    fixed create event

--- api/controllers/eventController.js
+++ api/controllers/eventController.js
@@ -8,11 +8,15 @@ const mongoose = require('mongoose'),
 // Create event
 exports.create_an_event = (req, res) => {
     let new_event = new Events(req.body);
-    if(new_event.date === null)
-    {
-        new_event.date = Date.now();
-    }
-    if(new_event.tag.access.includes(new_event.door.ObjectId))
+    new_event.in = req.body.event.in;
+    new_event.out = req.body.event.out;
+    new_event.door = req.body.event.door;
+    new_event.tag = req.body.event.tag;
+    new_event.tenant = req.body.event.tag.tenant;
+    new_event.access = req.body.event.access;
+    new_event.date = Date.now();
+
+    if(new_event.access.includes(new_event.door.ObjectId))
     {
         new_event.save((err, event) => {
             if (err)
@@ -127,11 +131,13 @@ exports.FindEntriesByLocation = (req, res) => {
        maxE = 20; 
     }
     let door = Door.find( {'door.location': req.params.location } );
+    
     Events.find( {'door': door.ObjectId} , (err, event) => {
         if(err)
         res.send(err);
     res.json(event);
     }).limit(maxE)
+    
 };
 
 exports.FindEntriesByTag = (req, res) => {
